e9ed5edd1e98ba7fb636ad2003def128fd8005e5
Román Gorojovsky <roman@monarlab.com>
2015-04-21 15:46:32 -0300

    Elimino nodos inútiles del AFD

--- ejercicio_d.py
+++ ejercicio_d.py
@@ -21,16 +21,16 @@ def interseccion(archivo_automata1, archivo_automata2, archivo_automata):
 
 	state_product = dict([((s1, s2), Node(name="(" + s1.name + ", " + s2.name + ")" )) for s2 in automata2.states for s1 in automata1.states])
 	
-	nodos_receptores = []
+	targeted_nodes = []
 	for (s1, s2) in state_product.keys():
 		for a in symbols:
 			if a in s1.transitions and a in s2.transitions:
 				state_product[(s1, s2)].add_transition(a, state_product[(s1.transitions[a], s2.transitions[a])])
-
-	# Limpiar nodos que no tienen transiciones asociadas?
+				targeted_nodes.append(state_product[(s1.transitions[a], s2.transitions[a])])
 
 	intersection_finals = [state_product[(s1, s2)] for (s1, s2) in state_product.keys() if s1 in automata1.finals and s2 in automata2.finals]
+	intersection_states = [s for s in state_product.values() if len(s.transitions) > 0 or s in targeted_nodes]
 
-	automata_interseccion = Automata(state_product[(automata1.initial, automata2.initial)], intersection_finals, symbols, state_product.values())
+	intersection_automata = Automata(state_product[(automata1.initial, automata2.initial)], intersection_finals, symbols, intersection_states)
 
-	return minimize(automata_interseccion)
+	return minimize(intersection_automata)
