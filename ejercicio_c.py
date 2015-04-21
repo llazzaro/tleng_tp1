228b2ea9d2270ceca5f1eff0af36a2d5f2e7f885
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2015-04-21 01:44:25 -0300

    ejercicio c

--- ejercicio_c.py
+++ ejercicio_c.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
+
+from parsers import load_automata
+from writers import save_dot
 
 
 def grafo(archivo_automata, archivo_dot):
-    raise NotImplementedError
+    automata = load_automata(archivo_automata)
+
+    save_dot(automata, archivo_dot)
