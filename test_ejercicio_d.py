3047e3cd06e76672f31c89ccc2da89976a8b599e
Román Gorojovsky <roman@monarlab.com>
2015-04-21 18:01:27 -0300

    Test para el ejercicio D

---
+++ test_ejercicio_d.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest import TestCase
+from StringIO import StringIO
+
+from ejercicio_d import interseccion
+
+class TestEjercicioD(TestCase):
+
+    def test_automatas_iguales(self):
+        input_automata = 'q0\tq1\n'
+        input_automata += '0\t1\n'
+        input_automata += 'q0\n'
+        input_automata += 'q1\n'
+        input_automata += 'q0\t0\tq1\n'
+        input_automata += 'q0\t1\tq1\n'
+        input_automata += 'q1\t0\tq0\n'
+        file_input1 = StringIO(input_automata)
+        file_input2 = StringIO(input_automata)
+        file_output = StringIO()
+        interseccion(file_input1, file_input2, file_output)
+
+        expected = '(q0,q0)\t(q1,q1)\n'
+        expected += '0\t1\n'
+        expected += '(q0,q0)\n'
+        expected += '(q1,q1)\n'
+        expected += '(q0,q0)\t0\t(q1,q1)\n'
+        expected += '(q0,q0)\t1\t(q1,q1)\n'
+        expected += '(q1,q1)\t0\t(q0,q0)\n'
+
+        import ipdb
+        ipdb.set_trace()
+
+if __name__ == '__main__':
+    unittest.main()
