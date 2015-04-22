c5ea1265c90f978677a3fd1357ed9b8e20fb028e
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2015-04-21 23:15:41 -0300

    fixeo unos bugs en el ejercicio c

--- test_ejercicio_c.py
+++ test_ejercicio_c.py
@@ -30,19 +30,17 @@ class TestEjercicioC(TestCase):
         file_out = StringIO()
         grafo(file_in, file_out)
 
-        expected = 'strict digraph {'
-        expected += 'rankdir=LR;'
-        expected += 'node [shape = none, label = "", width = 0, height = 0]; qd;'
-        expected += 'node [label="\N", width = 0.5, height = 0.5];'
-        expected += 'node [shape = doublecircle]; q1;'
-        expected += 'node [shape = circle]'
-        expected += 'qd -> q0'
-        expected += 'q0 -> q1 [label="a, b"]'
-        expected += 'q1 -> q1 [label="a, b"]'
+        expected = 'strict digraph {\n'
+        expected += 'rankdir=LR;\n'
+        expected += 'node [shape = none, label = "", width = 0, height = 0]; qd;\n'
+        expected += 'node [label="\N", width = 0.5, height = 0.5];\n'
+        expected += 'node [shape = doublecircle]; q1;\n'
+        expected += 'node [shape = circle];\n'
+        expected += 'qd -> q0\n'
+        expected += 'q0 -> q1 [label="a, b"]\n'
+        expected += 'q1 -> q1 [label="a, b"]\n'
         expected += '}'
 
-        import ipdb
-        ipdb.set_trace()
         file_out.seek(0)
         self.assertEquals(expected, file_out.read())
 
--- writers.py
+++ writers.py
@@ -1,19 +1,25 @@
+from collections import defaultdict
+
 
 def save_dot(automata, dot_file):
-    dot_file.write('digraph automata {')
-    dot_file.write('rankdir=LR;')
+    dot_file.write('strict digraph {\n')
+    dot_file.write('rankdir=LR;\n')
+    dot_file.write('node [shape = none, label = "", width = 0, height = 0]; qd;\n')
+    dot_file.write('node [label="\N", width = 0.5, height = 0.5];\n')
     for state in automata.states():
-        if state == automata.initial:
-            dot_file.write('node [shape = none, label = "", width = 0, height = 0]; {0}'.format(state.name))
-        elif state in automata.finals:
-            dot_file.write('"{0}" [shape = doublecircle]'.format(state.name))
-        else:
-            dot_file.write('"{0}" [shape=none]'.format(state.name))
-    dot_file.write('"" -> "{0}"'.format(automata.initial.name))
+        if state in automata.finals:
+            dot_file.write('node [shape = doublecircle]; {0};\n'.format(state.name))
+    dot_file.write('node [shape = circle];\n')
+    dot_file.write('qd -> {0}\n'.format(automata.initial.name))
+    arc_dict = defaultdict(list)
     for state in automata.states():
         for symbol, nodes in state.transitions.iteritems():
             for node in nodes:
-                dot_file.write('"{0}" -> "{1}" [label="{2}"]'.format(state.name, symbol, node.name))
+                arc_dict[(state.name, node.name)].append(symbol)
+
+    for nodes_tuple, symbols in arc_dict.iteritems():
+        symbols_comma = ', '.join(symbols)
+        dot_file.write('{0} -> {1} [label="{2}"]\n'.format(nodes_tuple[0], nodes_tuple[1], symbols_comma))
     dot_file.write('}')
 
 
