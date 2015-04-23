7d5a3e828e8a9c606454a2d9b1207a7ba2dfa7ee
Román Gorojovsky Sánchez <rgorojovsky@gmail.com>
2015-04-23 18:28:05 -0300

    Cambio cómo se definen los tests

--- test_ejercicio_d.py
+++ test_ejercicio_d.py
@@ -7,70 +7,69 @@ from ejercicio_d import interseccion, IncompatibleAlphabetsError, NonDeterminist
 class TestEjercicioD(TestCase):
 
     def test_automatas_iguales(self):
-        input_automata = 'q0\tq1\n'
-        input_automata += '0\t1\n'
+        input_automata =  '\t'.join(['q0', 'q1']) + '\n'
+        input_automata += '\t'.join(['0', '1']) + '\n'
         input_automata += 'q0\n'
         input_automata += 'q1\n'
-        input_automata += 'q0\t0\tq1\n'
-        input_automata += 'q0\t1\tq1\n'
-        input_automata += 'q1\t0\tq0\n'
+        input_automata += '\t'.join(['q0', '0', 'q1']) + '\n'
+        input_automata += '\t'.join(['q0', '1', 'q1']) + '\n'
+        input_automata += '\t'.join(['q1', '0', 'q0']) + '\n'
         file_input1 = StringIO(input_automata)
         file_input2 = StringIO(input_automata)
         file_output = StringIO()
         interseccion(file_input1, file_input2, file_output)
 
-        expected = '(q0,q0)\t(q1,q1)\n'
-        expected += '0\t1\n'
+        expected =  '\t'.join(['(q0,q0)', '(q1,q1)']) + '\n'
+        expected += '\t'.join(['0', '1']) + '\n'
         expected += '(q0,q0)\n'
         expected += '(q1,q1)\n'
-        expected += '(q0,q0)\t0\t(q1,q1)\n'
-        expected += '(q0,q0)\t1\t(q1,q1)\n'
-        expected += '(q1,q1)\t0\t(q0,q0)\n'
+        expected +=  '\t'.join(['(q0,q0)', '0', '(q1,q1)']) + '\n'
+        expected +=  '\t'.join(['(q0,q0)', '1', '(q1,q1)']) + '\n'
+        expected +=  '\t'.join(['(q1,q1)', '0', '(q0,q0)']) + '\n'
 
-        #import ipdb
-        #ipdb.set_trace()
 
     def test_alfabetos_disjuntos(self):
-        input_automata1 = 'q0\tq1\n'
-        input_automata1 += '0\t1\n'
+        input_automata1 =  '\t'.join(['q0', 'q1']) + '\n'
+        input_automata1 += '\t'.join(['0', '1']) + '\n'
         input_automata1 += 'q0\n'
         input_automata1 += 'q1\n'
-        input_automata1 += 'q0\t0\tq1\n'
-        input_automata1 += 'q0\t1\tq1\n'
-        input_automata1 += 'q1\t0\tq0\n'
+        input_automata1 += '\t'.join(['q0', '0', 'q1']) + '\n'
+        input_automata1 += '\t'.join(['q0', '1', 'q1']) + '\n'
+        input_automata1 += '\t'.join(['q1', '0', 'q0']) + '\n'
 
-        input_automata2 = 'q0\tq1\n'
-        input_automata2 += 'a\tb\n'
+        input_automata2 =  '\t'.join(['q0', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['a', 'b']) + '\n'
         input_automata2 += 'q0\n'
         input_automata2 += 'q1\n'
-        input_automata2 += 'q0\ta\tq1\n'
-        input_automata2 += 'q0\tb\tq1\n'
-        input_automata2 += 'q1\ta\tq0\n'
+        input_automata2 += '\t'.join(['q0', '0', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['q0', '1', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['q1', '0', 'q0']) + '\n'
 
         file_input1 = StringIO(input_automata1)
         file_input2 = StringIO(input_automata2)
         file_output = StringIO()
+
         with self.assertRaises(IncompatibleAlphabetsError):
             interseccion(file_input1, file_input2, file_output)
 
     def test_no_determinismo(self):
-        input_automata1 = 'q0\tq1\n'
-        input_automata1 += '0\t1\n'
+        input_automata1 =  '\t'.join(['q0', 'q1']) + '\n'
+        input_automata1 += '\t'.join(['0', '1']) + '\n'
         input_automata1 += 'q0\n'
         input_automata1 += 'q1\n'
-        input_automata1 += 'q0\t0\tq1\n'
-        input_automata1 += 'q0\t1\tq1\n'
-        input_automata1 += 'q1\t0\tq0\n'
-        input_automata1 += 'q1\t0\tq1\n'
-        input_automata1 += 'q1\t1\tq1\n'
+        input_automata1 += '\t'.join(['q0', '0', 'q1']) + '\n'
+        input_automata1 += '\t'.join(['q0', '1', 'q1']) + '\n'
+        input_automata1 += '\t'.join(['q1', '0', 'q0']) + '\n'
 
-        input_automata2 = 'q0\tq1\n'
-        input_automata2 += '0\t1\n'
+        input_automata2 =  '\t'.join(['q0', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['0', '1']) + '\n'
         input_automata2 += 'q0\n'
         input_automata2 += 'q1\n'
-        input_automata2 += 'q0\t0\tq1\n'
-        input_automata2 += 'q0\t1\tq1\n'
-        input_automata2 += 'q1\t0\tq0\n'
+        input_automata2 += '\t'.join(['q0', '0', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['q0', '1', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['q1', '0', 'q0']) + '\n'
+        input_automata2 += '\t'.join(['q1', '0', 'q1']) + '\n'
+        input_automata2 += '\t'.join(['q1', '1', 'q1']) + '\n'
 
         file_input1 = StringIO(input_automata1)
         file_input2 = StringIO(input_automata2)
