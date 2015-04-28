6e639aa36a17c0a31d53ecb92f6ac786d6c2a8d8
Román Gorojovsky <roman@monarlab.com>
2015-04-28 15:47:54 -0300

    Primero de una colección de scripts de testing/demo

---
+++ run_and_plot_ejercicio_d.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+FILES_DIR=files
+FILE_IN1=$FILES_DIR/empieza_010.aut
+FILE_IN2=$FILES_DIR/termina_010.aut
+FILE_OUT=$FILES_DIR/ejd.aut
+DOT_OUT=$FILES_DIR/ejd.dot
+PNG_OUT=$FILES_DIR/ejd.png
+
+python AFD.py -intersec -aut1 $FILE_IN1 -aut2 $FILE_IN2 -aut $FILE_OUT
+python AFD.py -aut $FILE_OUT -dot $DOT_OUT
+dot -Tpng $DOT_OUT -o $PNG_OUT
+eog $PNG_OUT
+
