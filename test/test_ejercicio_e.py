2c99bb03cadf4eb9d34a9bf8058467d93a2b9509
Román Gorojovsky <roman@monarlab.com>
2015-04-28 15:06:05 -0300

    test del ejercicio e

--- test/test_ejercicio_e.py
+++ test/test_ejercicio_e.py
@@ -2,7 +2,8 @@ import unittest
 from unittest import TestCase
 from StringIO import StringIO
 
-from ejercicio_e import complemento
+from parsers import load_automata
+from ejercicio_e import afd_complemento
 
 class TestEjercicioE(TestCase):
 
@@ -15,17 +16,13 @@ class TestEjercicioE(TestCase):
         input_automata += '\t'.join(['q0', '1', 'q1']) + '\n'
         input_automata += '\t'.join(['q1', '0', 'q0']) + '\n'
 
-        file_input1 = StringIO(input_automata)
-        file_output = StringIO()
-        complemento(file_input1, file_output)
+        automata = load_automata(StringIO(input_automata))
 
-        expected =  '\t'.join(['q0', 'q1']) + '\n'
-        expected += '\t'.join(['0', '1']) + '\n'
-        expected += 'q0\n'
-        expected += 'q0\n'
-        expected += '\t'.join(['q0', '0', 'q1']) + '\n'
-        expected += '\t'.join(['q0', '1', 'q1']) + '\n'
-        expected += '\t'.join(['q1', '0', 'q0']) + '\n'
+        automata_complemento = afd_complemento(automata)
+
+        self.assertEqual(automata.states(), automata_complemento.states())
+        self.assertEqual(automata.initial, automata_complemento.initial)
+        self.assertEqual(set(automata.states()) - automata.finals, automata_complemento.finals)
     
     
 
