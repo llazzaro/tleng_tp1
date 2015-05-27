92b33f1d06553ee5534ecbbd9013fa18e098cd31
Román Gorojovsky <roman@monarlab.com>
2015-05-27 15:56:45 -0300

    testeo otra cosa

--- run_and_plot_ejercicio_a.sh
+++ run_and_plot_ejercicio_a.sh
@@ -2,6 +2,7 @@
 
 FILES_DIR=files
 FILE_IN1=$FILES_DIR/4_3_arbol_ejemplo_1
+#FILE_IN1=$FILES_DIR/caso_ejemplo
 FILE_OUT=$FILES_DIR/eja.aut
 DOT_OUT=$FILES_DIR/eja.dot
 PNG_OUT=$FILES_DIR/eja.png
@@ -9,4 +10,4 @@ PNG_OUT=$FILES_DIR/eja.png
 python AFD.py -leng $FILE_IN1 -aut $FILE_OUT
 python AFD.py -aut $FILE_OUT -dot $DOT_OUT
 dot -Tpng $DOT_OUT -o $PNG_OUT
-eog $PNG_OUT
+eog $PNG_OUT &
